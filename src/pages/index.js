58f028de4cbe93dadfc762a2118fe9aa22f08c23
PatrickZ <pzarkha@hotmail.com>
2019-06-05 10:35:30 -0500

    fixing email feature

--- src/pages/index.js
+++ src/pages/index.js
@@ -121,10 +121,10 @@ class HomeIndex extends React.Component {
 
                     <section id="three">
                         <h2>Get In Touch</h2>
-                        <p>Feel free to send an e-mail if you would like to get in touch</p>
+                        <p>Feel free to call or send an e-mail if you would like to get in touch.</p>
                         <div className="row">
                             <div className="8u 12u$(small)">
-                                <form method="post" action="#">
+                                <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field" >
                                     <div className="row uniform 50%">
                                         <div className="6u 12u$(xsmall)"><input type="text" name="name" id="name" placeholder="Name" /></div>
                                         <div className="6u 12u$(xsmall)"><input type="email" name="email" id="email" placeholder="Email" /></div>
